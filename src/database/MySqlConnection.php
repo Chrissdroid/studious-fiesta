f251ef41cb9fadccca98d8c66b5928f8abb29002
hugo <hugo.lavernhe@gmail.com>
2021-02-04 12:31:41 +0100

    ajout MySqlConnection class et config

---
+++ src/database/MySqlConnection.php
@@ -0,0 +1,28 @@
+<?php
+require_once 'config.php';
+class MySqlConnection
+{
+    /** @var PDO */
+    private $_db;
+
+    public function __construct()
+    {
+        $this->set_db();
+    }
+
+    public function get_db()
+    {
+        return $this->_db;
+    }
+
+    private function set_db()
+    {
+        try {
+            $pdo =  new \PDO(DB_CONNEXION, DB_USERNAME, DB_PASSWORD);
+            $pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_WARNING);
+            $this->_db = $pdo;
+        } catch (PDOException $exception) {
+            return $exception;
+        }
+    }
+}
---
+++ src/database/config.php
@@ -0,0 +1,7 @@
+<?php
+DEFINE("SERVEUR", "localhost");
+DEFINE("PORT", "3306");
+DEFINE("DATABASE", "franceamusee");
+DEFINE("DB_USERNAME", "root");
+DEFINE("DB_PASSWORD", "toor");
+DEFINE("DB_CONNEXION", "mysql:host=" . SERVEUR . ":" . PORT . ";dbname=" . DATABASE);
