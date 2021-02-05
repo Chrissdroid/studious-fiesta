48148fe99aae4fbcc8f7ac8f761a59e7842ddb08
Youffie-WX <72621528+Youffie-WX@users.noreply.github.com>
2021-02-05 10:49:42 +0100

    add template card musees

--- public/musees.php
+++ public/musees.php
@@ -7,36 +7,50 @@
 <html lang="fr">
 
 <head>
-	<?php require '../src/includes/head.php'; ?>
-	<title>Bienvenue</title>
+    <?php require '../src/includes/head.php'; ?>
+    <title>Bienvenue</title>
 </head>
 
-    <body>
-        <?php require '../src/includes/header.php'; ?>
+<body>
+    <?php require '../src/includes/header.php'; ?>
 
-        <?php
+    <?php
 
-            $MuseeRepository = new MuseeRepository($bdd);
-            $musees = $MuseeRepository->selectAll();
+    $MuseeRepository = new MuseeRepository($bdd);
+    $musees = $MuseeRepository->selectAll();
 
-            foreach($musees as $musee ) {
-                $Musee = new Musee($musee);
-                ?>   
-                    <div class="card"> 
+    foreach ($musees as $musee) {
+        $Musee = new Musee($musee);
+    ?>
+        <div class="container mt-3">
+
+            <div class="card p-2 border border-4">
+                <div class="row">
+                    <div class="col">
+                        <img class="image-fluid mt-4 object-fit: cover; " style="width:300px; " src="<?= $Musee->get_photo() ?>" />
+                    </div>
+                    <div class="col col-9">
                         <h3><?= $Musee->get_nom() ?></h3>
-                        <img style="width:150px" src="<?= $Musee->get_photo() ?>"/>  
                         <p><?= $Musee->get_description() ?></p>
-                        <div>
-                        <p><?= $Musee->get_adresse() ?></p>
-                        <p><?= $Musee->get_ville() ?> <?= $Musee->get_cp() ?> </p>
-                        <p><?= $Musee->get_telephone()?> </p>
-                        <p><?= $Musee->get_site()?> </p>
+                        <div class="row mb-3">
+                            <div class="col">
+                                <p class="mb-0"><?= $Musee->get_adresse() ?></p>
+                                <p class="mb-0"><?= $Musee->get_ville() ?> <?= $Musee->get_cp() ?> </p>
+                                <p class="mb-0"><?= $Musee->get_telephone() ?> </p>
+                                <p class="mb-0"><?= $Musee->get_site() ?> </p>
+                            </div>
+                            <div class="col col-3 mt-4">
+                                <a href="#" class="btn btn-outline-secondary btn btn-primary stretched-link" style="width: 200px;">En savoir plus</a>
+                            </div>
                         </div>
-                    </div>                    
-                <?php
-                    };
-        ?>
-        <?php require '../src/includes/footer.php'; ?>
-    </body>
+                    </div>
+                </div>
+            </div>
+        </div>
+    <?php
+    };
+    ?>
+    <?php require '../src/includes/footer.php'; ?>
+</body>
 
 </html>
